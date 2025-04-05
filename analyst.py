9aed93fdb412e3f6604b42a4a8af90d2f07ccd22
Kiran Kulkarni <kk.questworld@gmail.com>
2025-04-05 16:07:21 +0530

    Add SQL functions supported by Polars

--- analyst.py
+++ analyst.py
@@ -82,10 +82,166 @@ def get_schema(
     return schema_dict
 
 
+polars_sql_aggregate_functions = [
+    "avg",
+    "count",
+    "first",
+    "last",
+    "max",
+    "median",
+    "min",
+    "sum",
+    "quantile_count",
+    "quantile_disc",
+    "stddev",
+    "sum",
+    "variance",
+]
+
+polars_sql_array_functions = [
+    "array_agg",
+    "array_contains",
+    "array_get",
+    "array_length",
+    "array_lower",
+    "array_mean",
+    "array_reverse",
+    "array_sum",
+    "array_to_string",
+    "array_unique",
+    "array_upper",
+    "unnest",
+]
+
+polars_sql_bitwise_functions = [
+    "bit_and",
+    "bit_count",
+    "bit_or",
+    "bit_xor",
+]
+
+polars_sql_conditional_functions = [
+    "coalesce",
+    "greatest",
+    "if",
+    "ifnull",
+    "least",
+    "nullif",
+]
+
+polars_sql_mathematical_functions = [
+    "abs",
+    "cbrt",
+    "ceil",
+    "div",
+    "exp",
+    "floor",
+    "ln",
+    "log2",
+    "log10",
+    "mod",
+    "pi",
+    "pow",
+    "round",
+    "sign",
+    "sqrt",
+]
+
+polars_sql_string_functions = [
+    "bit_length",
+    "concat",
+    "concat_ws",
+    "date",
+    "ends_with",
+    "initcap",
+    "left",
+    "length",
+    "lower",
+    "ltrim",
+    "normalize",
+    "octet_length",
+    "regexp_like",
+    "replace",
+    "reverse",
+    "right",
+    "rtrim",
+    "starts_with",
+    "strpos",
+    "strptime",
+    "substr",
+    "timestamp",
+    "upper",
+]
+
+polars_sql_temporal_functions = [
+    "date_part",
+    "extract",
+    "strftime",
+]
+
+polars_sql_type_functions = [
+    "cast",
+    "try_cast",
+]
+
+polars_sql_trigonometric_functions = [
+    "acos",
+    "acosd",
+    "asin",
+    "asind",
+    "atan",
+    "atand",
+    "atan2",
+    "atan2d",
+    "cot",
+    "cotd",
+    "cos",
+    "cosd",
+    "degrees",
+    "radians",
+    "sin",
+    "sind",
+    "tan",
+    "tand",
+]
+
+polars_sql_functions = {
+    "aggregate": polars_sql_aggregate_functions,
+    "array": polars_sql_array_functions,
+    "bitwise": polars_sql_bitwise_functions,
+    "conditional": polars_sql_conditional_functions,
+    "mathematical": polars_sql_mathematical_functions,
+    "string": polars_sql_string_functions,
+    "temporal": polars_sql_temporal_functions,
+    "type": polars_sql_type_functions,
+    "trigonometric": polars_sql_trigonometric_functions,
+}
+
+
+def gen_polars_sql_functions_str():
+    sql_functions_agg = []
+    for sql_fn_category, sql_fns in polars_sql_functions.items():
+        sql_fns_str = ["- " + sql_fn.capitalize() for sql_fn in sql_fns]
+        sql_fns_str = "\n".join(sql_fns_str)
+        sql_fn_with_header = f"{sql_fn_category.capitalize()}: \n{sql_fns_str}\n\n"
+        sql_functions_agg.append(sql_fn_with_header)
+    return "\n".join(sql_functions_agg)
+
+
+query_description = f"""
+The polars sql query to be executed.
+polars sql query must use the table name as `self` to refer to the source data.
+Supported functions are:
+{gen_polars_sql_functions_str()}
+"""
+
+
 @mcp.tool()
 def execute_polars_sql(
     file_locations: List[str],
-    query: str,
+    query: str = Field(
+        description=query_description,
+    ),
     file_type: str = Field(
         description="The type of the file to be read. Supported types are csv and parquet",
         default="csv",
